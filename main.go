4abdc7ee09bc60a10cc8722b56e4aa810dec88e9
Bert Hekman <demontpx@gmail.com>
2020-07-30 00:16:50 +0200

    Add actor loading example in main.go

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"github.com/demontpx/go-game/lib/actor"
 	"github.com/demontpx/go-game/lib/game"
 )
 
 func main() {
+	loadActor()
+	startGame()
+}
+
+func loadActor() {
+	loader := actor.NewLoader()
+	a, err := loader.LoadActorFromFile("res/actor/ball.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("actor loaded: %+v", a)
+}
+
+func startGame() {
 	main := game.NewMain()
 	err := main.Run()
 	if err != nil {
